35025cb125096a8982a25cc8944fae87ccc7b453
Majd Hafiri <majdhafiri@Majds-MacBook-Pro.local>
2021-11-16 00:10:25 +0100

    bubble sort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,1 +1,14 @@
-print("Hello world")
+
+##buble sort ---> O(n^2)
+listOfNumbers = [20,2,10,3.3,15,7]
+
+temp = 0 
+
+for i in range(len(listOfNumbers)):
+    for x in range (len(listOfNumbers) - i - 1):
+        if listOfNumbers[x] > listOfNumbers[x+1]:
+            temp = listOfNumbers[x+1]
+            listOfNumbers[x+1] = listOfNumbers[x]
+            listOfNumbers[x] = temp
+
+print(listOfNumbers)
