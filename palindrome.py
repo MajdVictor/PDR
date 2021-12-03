188996eeef0367d8e315a9716039a1232f1d21c8
Majd Hafiri <majdhafiri@Majds-MacBook-Pro.local>
2021-12-03 18:41:24 +0100

    palindrome using math

---
+++ palindrome.py
@@ -0,0 +1,20 @@
+#Finding Palindrome without usng strings
+
+def isPalindrome(x):
+        if x<0:
+            return False
+        else:
+            n = x
+            temp = 0
+            while n:
+                temp = temp*10 + n%10 
+                n//=10
+            return temp == x
+
+if __name__ == "__main__":
+
+    number = 234
+    number2 = 234432
+
+    print(isPalindrome(number))
+    print(isPalindrome(number2))
