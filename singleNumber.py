d44e9cb4f9547209756965c032d7f7cf836a661c
Majd Hafiri <majdhafiri@Majds-MacBook-Pro.local>
2021-12-07 14:37:16 +0100

    bitwise

--- singleNumber.py
+++ singleNumber.py
@@ -8,5 +8,17 @@ def singleNumber(listOfNumbers):
             del d[i]
     return list(d.keys())[0]
 
+def singleNumber2(listOfNumbers):
+
+    '''
+    This solution uses the XOR bit operation
+    '''
+    #bitwise operation ( XOR )
+    xorResult = 0
+        
+    for i in listOfNumbers:
+        xorResult = xorResult ^ i
+    return xorResult
+
 if __name__ == "__main__":
-    print(singleNumber([1,1,2,3,4,3,4]))
+    print(singleNumber2([1,1,2,3,4,3,4]))
