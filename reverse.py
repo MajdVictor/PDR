92f2e03701e057dd523287ec1b74bb008784b5d7
Majd Hafiri <majdhafiri@Majds-MacBook-Pro.local>
2021-12-20 21:30:45 +0100

    first solution

--- reverse.py
+++ reverse.py
@@ -1,20 +1,20 @@
 def reverseString(str):
 
-    #one solution
-    # str[:] = str[::-1]
-
-    # return str
-
-    #Second solution
-
-    start, end = 0, len(str)-1
-    while start<end:
-        str[start], str[end] = str[end], str[start]
-
-        start += 1
-        end -= 1
+    # one solution
+    str[:] = str[::-1]
 
     return str
+
+    # Second solution
+
+    # start, end = 0, len(str)-1
+    # while start<end:
+    #     str[start], str[end] = str[end], str[start]
+
+    #     start += 1
+    #     end -= 1
+
+    # return str
 
 if __name__ == "__main__":
     print(reverseString(['h','e','l','l','o']))
