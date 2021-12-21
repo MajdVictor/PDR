f82012d7775b04624adf8471e790aa9b2663e2e1
Majd Hafiri <majdhafiri@Majds-MacBook-Pro.local>
2021-12-21 19:49:37 +0100

    using set

--- isAnagram.py
+++ isAnagram.py
@@ -3,10 +3,12 @@ def isAnagram(s , t):
     if len(s) != len(t):
         return False
     else:
-        for i in s:
+        ##set is used since there's no need to count same letter multiple times if occured, -> best performance
+        for i in set(s):
             if s.count(i) != t.count(i):
                 return False
         return True
+
 
 if __name__ == "__main__":
     print(isAnagram("cat","tacd"))
